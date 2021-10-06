238ad70479c61507f81669b1ad675b02c7bc936f
codesg <sourabh.8.june.1996@gmail.com>
2021-10-06 16:07:36 +0530

    Mission3

--- src/main.cpp
+++ src/main.cpp
@@ -28,7 +28,7 @@ static void connectionStatusCallback(IOTHUB_CLIENT_CONNECTION_STATUS result,
 void ConnectIoTHub()
 {
   IoTHub_Init();
-  _device_ll_handle = IoTHubDeviceClient_LL_CreateFromConnectionString(CONN_STRING, MQTT_Protocol);
+  _device_ll_handle = IoTHubDeviceClient_LL_CreateFromConnectionString(CONNECTION_STRING, MQTT_Protocol);
   if(_device_ll_handle == NULL)
   {
     return;
@@ -58,16 +58,20 @@ void setup()
 	delay(1000);
     tft.begin();
     tft.setRotation(1);
-    tft.fillScreen(TFT_ORANGE);
+    tft.fillScreen(TFT_BLUE);
     tft.setTextColor(TFT_BLACK);
     tft.setTextSize(8);
-    tft.drawString("Hackathon ", 10,15);
-    tft.drawString("Counter: ", 17,65);
+    tft.drawString("Light ", 10,15);
+    tft.drawString("Value: ", 17,65);
     connectWiFi();
 
     //COnnect IoTHub
     initTime();
     ConnectIoTHub();
+
+    pinMode(WIO_LIGHT, INPUT);
+    pinMode(D0, OUTPUT);
+    pinMode(WIO_BUZZER, OUTPUT);
 }
 
 void sendTelemetry(const char *telemetry)
@@ -85,20 +89,33 @@ void loop()
 {
     IoTHubDeviceClient_LL_DoWork(_device_ll_handle);
 
+    int light = analogRead(WIO_LIGHT);
+    Serial.print("Light value: ");
+    Serial.println(light);
+
     DynamicJsonDocument doc(1024);
-    doc["counter"] = counter--;
+    doc["Light"] = light;
     string telemetry;
-    
-    //sprite buffer for temperature
-    spr.createSprite(100, 100); //create buffer
-    spr.fillSprite(TFT_WHITE); //fill background color
-    spr.setTextSize(25);
-    spr.setTextColor(TFT_BLACK);
-    spr.drawNumber(counter,20,20); //display value
-    spr.pushSprite(100,150); //push to LCD
-    spr.deleteSprite(); //clear buffer
     serializeJson(doc, telemetry);
     sendTelemetry(telemetry.c_str());
+    
+    //sprite buffer for light
+    spr.createSprite(200, 200); //create buffer
+    spr.fillSprite(TFT_WHITE); //fill background color
+    spr.setTextSize(15);
+    spr.setTextColor(TFT_BLACK);
+    spr.drawNumber(light,20,20); //display value
+    spr.pushSprite(100,150); //push to LCD
+    spr.deleteSprite(); //clear buffer
 
-    delay(5000);
+    delay(2000);
+    if (light < 100)
+    {
+        analogWrite(WIO_BUZZER, 0);
+    }
+    else
+    {
+        analogWrite(WIO_BUZZER, 140);
+    }
+    
 }
